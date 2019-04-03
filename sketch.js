8bd14d994ba62be28e9f283fa58d4b2d2d9206b1
Rebecca Keegan <rebeccachristinekeegan@gmail.com>
2019-04-03 10:54:31 +0100

    House

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 400)
+    background(255)
+}
+
+function draw() {
+    fill(255, 130, 0)
+    stroke (0)
+    triangle(0, 30, 40, 0, 80, 30)
+    rect(5, 30, 70, 70)
+}
